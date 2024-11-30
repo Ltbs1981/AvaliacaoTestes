414cb4397f2f760f3364d521aa32c86440a81fd9
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-11-29 21:27:56 -0300

    iniciado com a lógica

---
+++ FatorialTestes/Fatorando.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatorialTestes
+{
+    internal class Fatorando
+    {
+        public int NumeroInicial { get; set; }
+        public int Resultado { get; set; }
+
+        public int FatorarNumeros(int numInicial)
+        {
+            NumeroInicial = numInicial;
+            Resultado = 1;
+
+            for (int i = numInicial; i > 1; i--)
+            {
+                Resultado *= i;
+            }
+
+            return Resultado;
+        }
+    }
+}
---
+++ FatorialTestes/Program.cs
@@ -0,0 +1,8 @@
+﻿using FatorialTestes;
+
+
+
+Fatorando fatorando = new Fatorando();
+fatorando.NumeroInicial = 5;
+fatorando.FatorarNumeros(5);
+Console.WriteLine(fatorando.Resultado);
