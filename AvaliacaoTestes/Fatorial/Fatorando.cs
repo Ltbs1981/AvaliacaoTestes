1726501bbf1872726b9b6eb16dfca6774de1f2e1
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-12-05 21:28:53 -0300

    correção de arquivos e estrutura

---
+++ AvaliacaoTestes/Fatorial/Fatorando.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FatorialTestes
+{
+    public class Fatorando
+    {
+        public int CalcularFatorial(int numero)
+        {
+            if (numero < 0)
+            {
+                throw new ArgumentException("O número não pode ser negativo.");
+            }
+
+            if (numero == 0)
+            {
+                return 1;
+            }
+
+            int resultado = 1;
+
+            for (int i = numero; i > 1; i--)
+            {
+                resultado *= i;
+            }
+
+            return resultado;
+        }
+    }
+}
+
