7794052d074914ac141eeb1344ab94e1722b65b9
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-12-10 10:25:55 -0300

    NumeroTestes

---
+++ AvaliacaoTestes.Testes/NumeroTestes.cs
@@ -0,0 +1,84 @@
+﻿using AvaliacaoTestes.Numbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaliacaoTestes.Testes
+{
+    public class NumeroTestes
+    {
+        [Fact]
+        public void VerificarSequencia_DeveRetornarCrescente_QuandoSequenciaCrescente()
+        {
+            // Arrange
+            var numeros = new List<int> { 1, 2, 3, 4, 5 };
+
+            // Act
+            var resultado = Numero.VerificarSequencia(numeros);
+
+            // Assert
+            Assert.Equal("Crescente", resultado);
+        }
+
+        [Fact]
+        public void VerificarSequencia_DeveRetornarDecrescente_QuandoSequenciaDecrescente()
+        {
+            // Arrange
+            var numeros = new List<int> { 5, 4, 3, 2, 1 };
+
+            // Act
+            var resultado = Numero.VerificarSequencia(numeros);
+
+            // Assert
+            Assert.Equal("Decrescente", resultado);
+        }
+
+        [Fact]
+        public void VerificarSequencia_DeveRetornarNenhum_QuandoSequenciaDesordenada()
+        {
+            // Arrange
+            var numeros = new List<int> { 1, 3, 2, 4, 5 };
+
+            // Act
+            var resultado = Numero.VerificarSequencia(numeros);
+
+            // Assert
+            Assert.Equal("Nenhum", resultado);
+        }
+
+        [Fact]
+        public void VerificarSequencia_DeveRetornarNenhum_QuandoListaComApenasUmNumero()
+        {
+            // Arrange
+            var numeros = new List<int> { 1 };
+
+            // Act
+            var resultado = Numero.VerificarSequencia(numeros);
+
+            // Assert
+            Assert.Equal("Nenhum", resultado);
+        }
+
+        [Fact]
+        public void VerificarSequencia_DeveLancarArgumentNullException_QuandoListaForNula()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => Numero.VerificarSequencia(null));
+        }
+
+        [Fact]
+        public void VerificarSequencia_DeveRetornarNenhum_QuandoListaVazia()
+        {
+            // Arrange
+            var numeros = new List<int>();
+
+            // Act
+            var resultado = Numero.VerificarSequencia(numeros);
+
+            // Assert
+            Assert.Equal("Nenhum", resultado);
+        }
+    }
+}
---
+++ AvaliacaoTestes/Numbers/Numero.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaliacaoTestes.Numbers
+{
+    public class Numero
+    {
+        public static string VerificarSequencia(List<int> numeros)
+        {
+            if (numeros == null)
+            {
+                throw new ArgumentNullException(nameof(numeros), "A lista de números não pode ser nula.");
+            }
+
+            if (numeros.Count < 2)
+            {
+                return "Nenhum";
+            }
+
+            bool crescente = true;
+            bool decrescente = true;
+
+            for (int i = 1; i < numeros.Count; i++)
+            {
+                if (numeros[i] > numeros[i - 1])
+                {
+                    decrescente = false;
+                }
+                if (numeros[i] < numeros[i - 1])
+                {
+                    crescente = false;
+                }
+            }
+
+            if (crescente)
+            {
+                return "Crescente";
+            }
+
+            if (decrescente)
+            {
+                return "Decrescente";
+            }
+
+            return "Nenhum";
+        }
+    }
+}
